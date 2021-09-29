e4a75a51f689a721e77a9419330c9250cb7799b6
Matheus Laurindo <matheus104521@gmail.com>
2021-09-29 19:03:45 -0300

    Conclui requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe(`Testa se a página NotFound é renderizada se a url
+não é encontrada`, () => {
+  it('deveria renderizar NotFound e mostrar o texto NotFound', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const titleNotFound = screen.getByText(/Page requested not found/);
+
+    expect(titleNotFound).toBeInTheDocument();
+
+    const imgPikachuCrying = screen.getAllByRole('img');
+    expect(imgPikachuCrying[1]).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
